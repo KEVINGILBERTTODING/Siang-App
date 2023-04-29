04b66cbe20f0ff863eb0cd24d587ff2c5a98f8a0
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-04-29 15:24:48 +0700

    added login screen

--- app/src/main/java/com/example/siangapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/siangapp/SplashScreenActivity.java
@@ -23,7 +23,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
 
             }
         }, 1800L);
