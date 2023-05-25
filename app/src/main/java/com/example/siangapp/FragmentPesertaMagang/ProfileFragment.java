bb90d554ab858cf482f72d3932997a2089485181
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-05-25 14:57:17 +0700

    Added button logout

--- app/src/main/java/com/example/siangapp/FragmentPesertaMagang/ProfileFragment.java
+++ app/src/main/java/com/example/siangapp/FragmentPesertaMagang/ProfileFragment.java
@@ -1,5 +1,8 @@
 package com.example.siangapp.FragmentPesertaMagang;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +10,36 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.siangapp.LoginActivity;
 import com.example.siangapp.R;
 
 public class ProfileFragment extends Fragment {
+    Button btnLogOut;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        btnLogOut = view.findViewById(R.id.btnLogOut);
+
+        sharedPreferences = getContext().getSharedPreferences("user_data", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent(getContext(), LoginActivity.class));
+                getActivity().finish();
+
+            }
+        });
 
         return  view;
     }
